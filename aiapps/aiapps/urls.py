f12f39ee2402ce2b118730aca331d7876a87628a
h-funase <h.funse@gmail.com>
2020-02-07 01:15:04 +0900

    a

--- aiapps/aiapps/urls.py
+++ aiapps/aiapps/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import carbike.views as carbike
 
 urlpatterns = [
     path('admin/', admin.site.urls),
