a95a69bc6903c227c558d17f68af833d1e38e54d
h-funase <h.funse@gmail.com>
2020-02-11 15:00:18 +0900

    k

--- lovery_bot/lovery_bot/wsgi.py
+++ lovery_bot/lovery_bot/wsgi.py
@@ -15,3 +15,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lovery_bot.settings')
 
 application = get_wsgi_application()
 
+# ファイルの末尾に以下を追記
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
